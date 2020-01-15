67869e57475803e6265cfdd2c613f4a13f3b9d29
Naeem <naeemukhan14@gmail.com>
2020-01-15 15:50:06 +0400

    Base setup complete

---
+++ Home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class HomePage(TemplateView):
+    template_name = 'home/index.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {})
+
