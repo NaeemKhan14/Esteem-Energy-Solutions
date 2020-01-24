0de6544940406b09fafd2544b574d02a880caf24
Akilan <Akilanselva@hotmail.com>
2020-01-24 17:48:34 +0400

    Models added

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+class room(models.Model):
+    room_no = models.IntegerField(primary_key=True)
+    room_name = models.TextField(max_length = 40)
+
+class plugs(models.Model):
+    plug_no = models.IntegerField(primary_key=True)
+    plug_name = models.TextField(max_length = 40)
+    plug_model_name = models.TextField(max_length = 40)
+    room_no = models.ForeignKey(room, default=1, on_delete=models.CASCADE)
+
+class plug_config(models.Model):
+    plug_no = models.ForeignKey(plugs, default=1, on_delete=models.CASCADE)
+    current_power = models.TextField()
+    turn_on = models.TextField()
+    turn_off = models.TextField()
+
+class plug_electricity_consumption(models.Model):
+    plug_no = models.ForeignKey(plugs, default=1,on_delete=models.CASCADE)
+    Watt = models.DecimalField(max_digits=20, decimal_places=2) 
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+# Second part 
+class energy_generation(models.Model):
+    e_id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+
+class battery(models.Model):
+    e_id = models.ForeignKey(energy_generation, default=1,on_delete=models.CASCADE)
+    capacity = models.DecimalField(max_digits=20, decimal_places=2) 
+    current_charge = models.DecimalField(max_digits=3, decimal_places=2) 
+    current_capacity = models.DecimalField(max_digits=20, decimal_places=2)
+    maximum_capacity = models.DecimalField(max_digits=20, decimal_places=2) 
+
+class power_transaction(models.Model):
+    e_id = models.ForeignKey(energy_generation, default=1,on_delete=models.CASCADE)
+    transfer = models.DecimalField(max_digits=20, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class power_generation: 
+    e_id = models.ForeignKey(energy_generation, default=1,on_delete=models.CASCADE)
+    current_power = models.DecimalField(max_digits=20, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+
+
+
