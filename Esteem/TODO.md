1194964ed81896694046247e844c8e5e11a96f52
Naeem Khan <naeemukhan14@gmail.com>
2020-02-04 15:27:40 +0400

    Made the flask server to randomly select an item and change it at given intervals.

--- Esteem/TODO.md
+++ Esteem/TODO.md
@@ -1,8 +1,6 @@
 # Todo list 
-
-1. Intergrate with Flask 
-2. Writing a script that reads every 2 minutes from API and stores in the DB
-3. Using API and DB for Chart js
+ 
+2. Using API and DB for Chart js
                 1. Create a Ajax request to update graph 
                 2. Displays the graph based on the parameter from the GET request 
                 3. Live data : Direct call from API
--- Flask_server/sample_server.py
+++ Flask_server/sample_server.py
@@ -1,5 +1,7 @@
 import flask
-from flask import request, jsonify
+from flask import jsonify
+from threading import Timer
+from random import randint
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -7,28 +9,31 @@ app.config["DEBUG"] = True
 # Create some test data for our catalog in the form of a list of dictionaries.
 kawaii = [
     {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
+     'name': 'Smart Plug',
+     'power_consumption': '10 Watts',
+     'Status': 'Off'},
     {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
+     'name': 'Smart Light',
+     'power_consumption': '20 Watts',
+     'Status': 'On'},
     {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
+     'name': 'Air Conditioner',
+     'power_consumption': '69 Watts',
+     'Status': 'Off'}
 ]
 
+# Selects a random item's Status from the list and changes it depending on its state.
+def randomize_data(interval):
+    global kawaii
+    Timer(interval, randomize_data, [interval]).start()
+    index = randint(0, len(kawaii) - 1)
+    if kawaii[index]['Status'] == 'Off':
+        kawaii[index]['Status'] = 'On'
+    else:
+        kawaii[index]['Status'] = 'Off'
 
-@app.route('/', methods=['GET'])
-def home():
-    return '''<h1>Sample text</h1>
-<p>Kawii Solutions</p>'''
-
+# Change status every second.
+randomize_data(1)
 
 # A route to return all of the available entries in our catalog.
 @app.route('/api/v1/kawaii', methods=['GET'])
