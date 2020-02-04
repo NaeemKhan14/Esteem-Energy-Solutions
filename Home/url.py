7139f4f03acf7eeab34bfef2c51bfbf239edbdbb
Naeem Khan <naeemukhan14@gmail.com>
2020-02-04 21:01:43 +0400

    Fixed the frontend and urls to match with the changes made in backend

--- Home/url.py
+++ Home/url.py
@@ -1,10 +1,9 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
-from .views import HomePage,EnergyGeneration,RoomPage,Rooms,Plugs
+from .views import HomePage, EnergyGeneration, RoomPage
 
 urlpatterns = [
     path('', HomePage.as_view(), name='homepage'),
     path('energy', EnergyGeneration.as_view(), name='energygeneration'),
-    path('room', RoomPage.as_view(), name='roompage'),
-    path('room_action', Rooms.as_view(), name='rooms'),
-    path('plugs',Plugs.as_view(), name="plugs")
+    path('room/<int:room_no>/', RoomPage.as_view(), name='roompage'),
 ]
