a5b4c7a1608884bbdd5dd71b2113281c48c34adc
akilan <akilanselva@hotmail.com>
2020-04-14 12:46:33 +0400

    fixed basic bug

--- Home/views.py
+++ Home/views.py
@@ -241,9 +241,8 @@ class Rooms(TemplateView):
                 l = (list(plug_electricity_consumption.objects.filter(plug_no=plug_no,timestamp__day=day,timestamp__month=month,timestamp__year=year,timestamp__hour=k)))
                 for j in l:
                    sum += j.Watt
-                   hourly_data_plug.append({'hour':k,'Watts':sum})
-                print(len(hourly_data_plug))
-                hourly_data_tot_plug.append(hourly_data_plug)
+                hourly_data_plug.append({'hour':k,'Watts':sum})
+             hourly_data_tot_plug.append(hourly_data_plug)
 
         for i in range(hour):
             sum = 0
